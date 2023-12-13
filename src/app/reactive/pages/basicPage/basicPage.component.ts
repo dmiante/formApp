8923fb9efcf232d363d5c4bd06042e6e0918c5ce
dmiante <dmi4ndev@gmail.com>
2023-12-13 19:25:00 -0300

    ADD code reactive forms angular

---
+++ src/app/reactive/pages/basicPage/basicPage.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX5090',
+  price: 2500,
+  inStorage: 6
+}
+
+@Component({
+  selector: 'app-basic-page',
+  templateUrl: './basicPage.component.html',
+  styleUrls: ['./basicPage.component.css'],
+})
+export class BasicPageComponent implements OnInit {
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  //   price: new FormControl(0, [Validators.required, Validators.min(0)]),
+  //   inStorage: new FormControl(0, [Validators.required, Validators.min(0)]),
+  // });
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    // this.myForm.reset(rtx5090)
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: string): string | null{
+    if(!this.myForm.controls[field]) return null
+
+    const errors = this.myForm.controls[field].errors || {}
+
+    for (const key of Object.keys(errors)) {
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido'
+        case 'minlength':
+          return `Minimo ${errors['minlength'].requiredLength} caracteres.`
+      }
+    }
+
+    return null
+  }
+
+  onSave(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
+  }
+}
---
+++ src/app/reactive/reactive.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { ReactiveRoutingModule } from './reactive-routing.module';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
+import { BasicPageComponent } from './pages/basicPage/basicPage.component';
+
+
+@NgModule({
+  declarations: [
+    BasicPageComponent,
+    DynamicPageComponent,
+    SwitchesPageComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveRoutingModule,
+    ReactiveFormsModule
+  ]
+})
+export class ReactiveModule { }
