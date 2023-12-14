aaa5dcf10f26eb671e6db9a23b50a70be01d1d1c
dmiante <dmi4ndev@gmail.com>
2023-12-14 20:51:29 -0300

    ADD validators email and password duplicate

--- src/app/auth/pages/registerPage/registerPage.component.ts
+++ src/app/auth/pages/registerPage/registerPage.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidatorsService } from 'src/app/shared/services/validators.service';
+import { EmailValidatorService } from 'src/app/shared/validators/email-validator.service';
 import * as customValidators from 'src/app/shared/validators/validators';
 
 @Component({
@@ -12,24 +13,41 @@ export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
     name: [
       '',
-      [Validators.required, Validators.pattern(this.validatorService.firstNameAndLastnamePattern)],
+      [
+        Validators.required,
+        Validators.pattern(this.validatorService.firstNameAndLastnamePattern),
+      ],
     ],
+    // email: [
+    //   '',
+    //   [Validators.required, Validators.pattern(this.validatorService.emailPattern)],
+    // [new EmailValidator]
+    // ],
     email: [
       '',
-      [Validators.required, Validators.pattern(this.validatorService.emailPattern)],
+      [
+        Validators.required,
+        Validators.pattern(this.validatorService.emailPattern),
+      ],
+      [this.emailValidator],
     ],
     username: ['', [Validators.required, this.validatorService.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
+  }, {
+    validators: [
+      this.validatorService.isFieldOneEqualFieldTwo('password', 'password2')
+    ]
   });
 
   constructor(
     private fb: FormBuilder,
-    private validatorService: ValidatorsService
-    ) {}
+    private validatorService: ValidatorsService,
+    private emailValidator: EmailValidatorService
+  ) {}
 
   isValidField(field: string) {
-    return this.validatorService.isValidField(this.myForm, field)
+    return this.validatorService.isValidField(this.myForm, field);
   }
 
   onSubmit() {
