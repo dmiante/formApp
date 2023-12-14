162f1b3b9eb26dc1c410c2f0a9d713d13d0f26b7
dmiante <dmi4ndev@gmail.com>
2023-12-13 22:26:11 -0300

    ADD register page logic with validators

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterPageComponent } from './pages/registerPage/registerPage.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterPageComponent
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/pages/registerPage/registerPage.component.ts
+++ src/app/auth/pages/registerPage/registerPage.component.ts
@@ -1,14 +1,38 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/services/validators.service';
+import * as customValidators from 'src/app/shared/validators/validators';
 
 @Component({
   selector: 'app-register-page',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
   templateUrl: './registerPage.component.html',
   styleUrls: ['./registerPage.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterPageComponent { }
+export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: [
+      '',
+      [Validators.required, Validators.pattern(this.validatorService.firstNameAndLastnamePattern)],
+    ],
+    email: [
+      '',
+      [Validators.required, Validators.pattern(this.validatorService.emailPattern)],
+    ],
+    username: ['', [Validators.required, this.validatorService.cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private validatorService: ValidatorsService
+    ) {}
+
+  isValidField(field: string) {
+    return this.validatorService.isValidField(this.myForm, field)
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+}
